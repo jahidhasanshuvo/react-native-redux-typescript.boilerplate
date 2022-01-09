96ea7732ab80f8b33736689c0905ea1e27e9797c
jahidshuvoml <jahid.shuvo@monstar-lab.com>
2022-01-10 00:35:18 +0600

    redux persist added for store application data

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,36 @@ import {StatusBar} from 'react-native';
 import BottomTab from './screens/BottomTab';
 import Login from './screens/Login';
 import {Screens} from './screens/Screens';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar backgroundColor={'#009999'} />
-        <Stack.Navigator
-          screenOptions={{
-            headerTitleAlign: 'center',
-            animation: 'slide_from_right',
-            headerStyle: {backgroundColor: '#009999'},
-          }}
-          initialRouteName={Screens.Login}>
-          <Stack.Screen
-            name={Screens.Login}
-            component={Login}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name={Screens.BottomTab}
-            component={BottomTab}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <NavigationContainer>
+          <StatusBar backgroundColor={'#009999'} />
+          <Stack.Navigator
+            screenOptions={{
+              headerTitleAlign: 'center',
+              animation: 'slide_from_right',
+              headerStyle: {backgroundColor: '#009999'},
+            }}
+            initialRouteName={Screens.Login}>
+            <Stack.Screen
+              name={Screens.Login}
+              component={Login}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name={Screens.BottomTab}
+              component={BottomTab}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,21 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {useDispatch} from 'react-redux';
 import rootReducer from './root.reducer';
-
-export type RootState = ReturnType<typeof rootReducer>;
+import {persistReducer} from 'redux-persist';
+import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
 
